e7d10b5bc93e847a4ab803774c705720a848b1e4
Jonathan Ordoñez <je.ordonez.2017@alumnos.urjc.es>
2018-03-19 00:30:46 +0100

    Main -Jugador y Manager
    Creación de 3 jugadores y 1 manager en la clase Main - mensajes de listo

--- src/main/java/com/masterinformatica/bingo/main/Main.java
+++ src/main/java/com/masterinformatica/bingo/main/Main.java
@@ -1,25 +1,135 @@
 package com.masterinformatica.bingo.main;
 
-import com.masterinformatica.barcos.actors.Manager;
 import com.masterinformatica.bingo.entities.Bombo;
 import com.masterinformatica.bingo.entities.Carton;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+import akka.event.Logging;
+import akka.event.LoggingAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 	
-    public static void main(String[] args) {    	
-    	Bombo bombo = new Bombo();
+	public static ActorSystem actorSystem;
+	public static ActorRef Jugador1;
+	public static ActorRef Jugador2;
+	public static ActorRef Jugador3;
+	public static ActorRef Manager1;
+	
+	
+	
+	
+	static public class Jugador extends UntypedActor {
+		
+		//Mensajes
+		public enum msj {LISTO, LINEA, BINGO};
+		
+
+		private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+		
+		private Carton carton;
+		
+	    @Override
+	    public void preStart() {
+	    	this.carton = new Carton();
+	    	
+	    }
+
+	    @Override
+	    public void onReceive(Object message)  throws Exception{
+	    	log.info("[Jugador '{}'] ha enviado el mensaje: \"{}\".", getSelf().toString(), message);
+	    	
+	    	if(message == msj.LISTO){
+	    		Manager1.tell(Manager.msj.JUGADOR_LISTO, getSelf());
+	    		System.err.println(carton.toString());
+	        }else{
+	            unhandled(message);
+	        }
+	    	
+	    }
+	    
+	    @Override 
+	    public void unhandled(Object message) {
+	    	log.error("Message not registered");
+	    }
+	    
+	}
+	
+	
+	
+	static public class Manager extends UntypedActor {
+		
+		//Mensajes
+		public enum msj {JUGADOR_LISTO};
+		
+		//Lista de Jugadores
+		private List<ActorRef> Jugadores;
+
+		private final LoggingAdapter log = Logging.getLogger(getContext().system(), this);
+
+		private Bombo bombo;
+		
+	    @Override
+	    public void preStart() {
+	    	this.bombo = new Bombo();
+	    	//Instancia de jugadores
+	    	Jugadores = new ArrayList<>();
+	    	System.err.println(bombo.toString());
+	    	
+	    }
+
+	    @Override
+	    public void onReceive(Object message)  throws Exception{
+
+	    	log.info("[Manager '{}'] ha recibido el mensaje: \"{}\".", getSelf().toString(), message);
+	    	
+	    	//El Manager recibirá in LISTO de todos los jugadores y los agrega a la lista
+	        if (message == Manager.msj.JUGADOR_LISTO) {
+	        	Jugadores.add(getSender());
+
+                if(!Jugadores.isEmpty()){
+                	Thread.sleep(3500);
+                	System.err.println("Hay jugadores para empezar");
+                	//Si hay mas de un jugador empiece a cantar los números
+	
+                }
+
+	            }else{
+	                unhandled(message);
+	            }
+	    }
+	        
+	    @Override 
+	    public void unhandled(Object message) {
+	    	log.error("Message not registered");
+	    }
+	}
+
+	
+    public static void main(String[] args) throws InterruptedException{  
+    	
+    	/*Bombo bombo = new Bombo();
     	Carton carton = new Carton();
     	
     	System.err.println(bombo.toString());
-    	System.err.println(carton.toString());
-    	
-    	akka.Main.main(new String[]{Manager.class.getName()});
+    	System.err.println(carton.toString());*/
+
+    	actorSystem = ActorSystem.create("ActorSystem");
+    	Manager1 = actorSystem.actorOf(Props.create(Manager.class), "Manager");
+        Jugador1 = actorSystem.actorOf(Props.create(Jugador.class), "Jugador1");
+        Jugador2 = actorSystem.actorOf(Props.create(Jugador.class), "Jugador2");
+        Jugador3 = actorSystem.actorOf(Props.create(Jugador.class), "Jugador3");
+
+        Jugador1.tell(Jugador.msj.LISTO, ActorRef.noSender());
+        Jugador2.tell(Jugador.msj.LISTO, ActorRef.noSender());
+        Jugador3.tell(Jugador.msj.LISTO, ActorRef.noSender());
+        
+        akka.Main.main(new String[]{});
     }
     
 }
